d7886f954a2cca4087940ff4a6b5c092f1ed6c8c
max2556 <46759632+max2556@users.noreply.github.com>
2022-08-20 01:37:41 +0300

    Lastworks: arrow buttons, autoslide, hover effects

--- js/slider.js
+++ js/slider.js
@@ -6,12 +6,14 @@ const CONFIG = {
   skip: {
     once_count: 2,
   },
+  autoscrollInterval: 3000
 }
 
 const CURRENT_TYPE = TYPES.skip
 
 const STATE = {
   last_clicked: null,
+  buttons: null,
 }
 
 //const controlButtons = document.querySelectorAll('.scroll-interface .move')
@@ -36,8 +38,14 @@ function init() {
       //controlFunc = step
       break
   }
+
+  prepareArrowButtons()
+  setInterval(autoscroll, CONFIG.autoscrollInterval);
 }
 
+/**
+ * Подготавливает слайдер в режиме переключения фиксированного количества блоков(пропускает 2 и более)
+ */
 function prepareSkip() {
   const slides = Math.ceil(blocks.length / CONFIG.skip.once_count)
   const callback = skip
@@ -53,10 +61,19 @@ function prepareSkip() {
       button.classList.add('active')
     })
   }
+
+  STATE.buttons = buttons
 }
 
+/**
+ * Создает кнопки управления слайдером
+ *
+ * @param {int} count Количество кнопок
+ * @returns массив кнопок
+ */
 function generateButtons(count) {
-  const parent = document.querySelector('.scroll-interface')
+  const buttonsParentSelector = '.slider-control .buttons'
+  const parent = document.querySelector(buttonsParentSelector)
   for (let i = 0; i < count; i++) {
     let newButton = document.createElement('button')
     newButton.classList.add('move')
@@ -68,4 +85,43 @@ function generateButtons(count) {
   STATE.last_clicked = parent.children[0]
   parent.children[0].classList.add('active')
   return parent.children
+}
+
+/**
+ * Задает логику для крайних кнопок - стрелок, смещающих положение на один в нужную сторону
+ */
+function prepareArrowButtons() {
+  const leftButton = document.querySelector('.slider-control .move-arrow.left')
+  const rightButton = document.querySelector(
+    '.slider-control .move-arrow.right',
+  )
+
+  if (!leftButton || !rightButton) {
+    console.warn(
+      `Could not find arrow buttons!\nMake sure that your index.html has <button> tags with .move-arrow.left and .move-arrow.right classes!`,
+    )
+    return
+  }
+
+  leftButton.addEventListener('click', () => {
+    let currentId = STATE.last_clicked.id
+    let newId = parseInt(currentId) - 1
+    if (newId >= 0) STATE.buttons[newId].click()
+  })
+
+  rightButton.addEventListener('click', () => {
+    let currentId = STATE.last_clicked.id
+    let newId = parseInt(currentId) + 1
+    if (newId < STATE.buttons.length) STATE.buttons[newId].click()
+  })
+}
+
+/**
+ * Функция автоскролла, вызывается в init() каждые CONFIG.autoscrollInterval милисекунд
+ */
+function autoscroll() {
+  let currentId = STATE.last_clicked.id
+  let newId = parseInt(currentId) + 1
+  if (newId >= STATE.buttons.length) newId = 0
+  STATE.buttons[newId].click()
 }
