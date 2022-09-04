fc1926a6f0608ea66a1ca3f286ad56c8065c209a
Preisy <preus_3@mail.ru>
2022-09-04 19:15:49 +0300

    added animation

---
+++ js/ourskills.ts
@@ -0,0 +1,51 @@
+let lines: NodeListOf<SVGPathElement> = document.querySelectorAll(".our_skills .skill_wrapper:not(:last-of-type) .skill_line path")
+let skills: NodeListOf<HTMLDivElement> = document.querySelectorAll(".our_skills .skill")
+let transitionDur: number = 500;
+let transitionInterval: number = 1000;
+let iterationInterval: number = 1000;
+
+for (let i = 0; i < skills.length; i++) {
+    skills[i].style.animationDelay = i * transitionInterval + "ms";
+}
+
+for (let i = 0; i < lines.length; i++) {
+    lines[i].style.strokeDasharray = lines[i].getTotalLength() + "px";
+    lines[i].style.strokeDashoffset = lines[i].getTotalLength() + "px";
+    lines[i].style.animationDelay = i * transitionInterval + "ms";
+}
+
+function animSkills(): void {
+    for (let i = 0; i < skills.length; i++) {
+        skills[i].classList.add("active_skill")
+    }
+    setTimeout(function () {
+        for (let i = 0; i < skills.length; i++) {
+            skills[i].classList.remove("active_skill")
+        }
+        setTimeout(animSkills, iterationInterval)
+    }, transitionInterval * skills.length)
+
+}
+
+function animLines(): void {
+    let i = 0;
+    (function setSDO() {
+        let length: number = lines[i].getTotalLength();
+        lines[i].style.setProperty("stroke-dashoffset", length * 2 + "px", "important");
+        (function (el: SVGPathElement = lines[i]) {
+            setTimeout(function () {
+                el.style.setProperty("transition", "none");
+                el.style.setProperty("stroke-dashoffset", length + "px")
+                el.getBoundingClientRect();
+                el.style.removeProperty("transition");
+            }, transitionInterval)
+        })();
+        ++i;
+        if (i < lines.length)
+            setTimeout(setSDO, transitionInterval);
+    })();
+    setTimeout(animLines, iterationInterval + transitionInterval * skills.length)
+}
+
+animSkills();
+animLines();
