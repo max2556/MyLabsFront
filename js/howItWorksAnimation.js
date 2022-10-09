5cfc1899db6608822c433367c6a5a843fa79d2cf
max2556 <46759632+max2556@users.noreply.github.com>
2022-10-09 16:51:01 +0300

    replace with real text

--- js/howItWorksAnimation.js
+++ js/howItWorksAnimation.js
@@ -10,9 +10,17 @@
         //data-wow-duration="0s" data-wow-delay="0s"
         const anim_len = 1.25
         const half_height = window.innerHeight / 2;
+        const offset = 200;
 
-        step.setAttribute('data-wow-offset', half_height)
+        const size = step.getBoundingClientRect();
+        const margin = i % 2 == 0 ? size.width / 2 : -1 * size.width / 2;
+
+
+
+        step.setAttribute('data-wow-offset', offset)
         step.setAttribute('data-wow-delay', `${anim_len * 0}s`)
         step.setAttribute('data-wow-duration', `${anim_len}s`)
+
+        step.setAttribute('style', `margin-left:${margin}px`)
     })
 })()
