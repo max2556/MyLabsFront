830663ef99166f918069361605190b358465ea27
max2556 <46759632+max2556@users.noreply.github.com>
2022-10-28 01:01:56 +0300

    FAQ content fill

---
+++ js/faqList.js
@@ -0,0 +1,29 @@
+(function faqInit() {
+    const faq_holder = document.querySelector('.about-us .FAQ');
+    const block_elements = faq_holder.querySelectorAll('.block');
+    const question_elements = faq_holder.querySelectorAll('.question');
+
+
+    for(let question of question_elements){
+        question.onclick = () => {
+            const parent = question.parentElement;
+            const answer = parent.querySelector('.answer');
+
+            const questionHeight = question.getBoundingClientRect().height+10;
+            const spanSize = answer.querySelector('span').getBoundingClientRect().height;
+            const answerSize = answer.getBoundingClientRect().height;
+
+            const maxHeight = Math.max(spanSize, answerSize)+20;
+
+            question.classList.toggle('active');
+
+            const isActive = question.classList.contains('active');
+            const delta = isActive ? maxHeight : 5;
+
+            const answerTranslate = isActive ? questionHeight : 0;
+
+            parent.setAttribute('style', `margin-bottom:${delta}px`);
+            answer.setAttribute('style', `transform: translateY(${answerTranslate}px)`);
+        }
+    }
+})();
